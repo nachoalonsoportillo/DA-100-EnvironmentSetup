d7e7d116be5a470708058d3f3c33eefa7828c05f
nachoalonsoportillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2022-03-04 20:47:09 +0100

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -1,1 +1,38 @@
+$chocolateyAppList = "az.powershell,azure-cli,sql-server-management-studio,git,sql-server-2019 -y --params=""'/IgnorePendingReboot /INSTANCENAME=MSSQLSERVER /BROWSERSVCSTARTUPTYPE=Automatic'"",powerbi,powerbi-reportbuilder"
 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){   
+    Write-Host "Chocolatey Apps Specified"  
+    
+    $appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+
+    foreach ($app in $appsToInstall)
+    {
+        Write-Host "Installing $app"
+        & choco install $app /y
+    }
+}
+Write-Host "Enable SQL TCP"
+$env:PSModulePath = $env:PSModulePath + ";C:\Program Files (x86)\Microsoft SQL Server\150\Tools\PowerShell\Modules"
+Import-Module -Name "sqlps"
+$smo = 'Microsoft.SqlServer.Management.Smo.'  
+$wmi = new-object ($smo + 'Wmi.ManagedComputer').  
+# List the object properties, including the instance names.  
+$Wmi
+
+# Enable the TCP protocol on the default instance.  
+$uri = "ManagedComputer[@Name='" + (get-item env:\computername).Value + "']/ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Tcp']" 
+$Tcp = $wmi.GetSmoObject($uri)  
+$Tcp.IsEnabled = $true  
+$Tcp.Alter()  
+$Tcp
+
+# Enable the named pipes protocol for the default instance.  
+$uri = "ManagedComputer[@Name='" + (get-item env:\computername).Value + "']/ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Np']"  
+$Np = $wmi.GetSmoObject($uri)  
+$Np.IsEnabled = $true  
+$Np.Alter()  
+$Np
+
+Restart-Service -Name 'MSSQLSERVER'
