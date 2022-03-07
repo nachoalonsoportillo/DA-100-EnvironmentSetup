b89723297fe98fff04685c1ece8f804ceac28bca
nachoalonsoportillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2022-03-07 09:38:57 +0100

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -52,7 +52,6 @@ Set-Location D:\
 
 Remove-Item -Path D:\temp -Recurse -Force
 
-# Creating PowerShell Logon Script
 $LogonScript = @'
 Start-Transcript -Path C:\tmp\LogonScript.log
 
@@ -67,16 +66,16 @@ Write-Host "Enable SQL TCP"
 $env:PSModulePath = $env:PSModulePath + ";C:\Program Files (x86)\Microsoft SQL Server\150\Tools\PowerShell\Modules"
 Import-Module -Name "sqlps"
 $smo = 'Microsoft.SqlServer.Management.Smo.'  
-$wmi = new-object ($smo + 'Wmi.ManagedComputer').  
-# List the object properties, including the instance names.  
+$wmi = new-object ($smo + 'Wmi.ManagedComputer').
+
 $Wmi
-# Enable the TCP protocol on the default instance.  
+
 $uri = "ManagedComputer[@Name='" + (get-item env:\computername).Value + "']/ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Tcp']" 
 $Tcp = $wmi.GetSmoObject($uri)  
 $Tcp.IsEnabled = $true  
 $Tcp.Alter()  
 $Tcp
-# Enable the named pipes protocol for the default instance.  
+
 $uri = "ManagedComputer[@Name='" + (get-item env:\computername).Value + "']/ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Np']"  
 $Np = $wmi.GetSmoObject($uri)  
 $Np.IsEnabled = $true  
@@ -86,10 +85,8 @@ Restart-Service -Name 'MSSQLSERVER'
 Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$False
 '@ > C:\tmp\LogonScript.ps1
 
-# Creating LogonScript Windows Scheduled Task
 $Trigger = New-ScheduledTaskTrigger -AtLogOn
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument 'C:\tmp\LogonScript.ps1'
 Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User "${adminUsername}" -Action $Action -RunLevel "Highest" -Force
 
-# Disabling Windows Server Manager Scheduled Task
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
